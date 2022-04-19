fe1cd0364993b18e00edb09497c48c68a2838c59
sanket jethava <74979818+sanket-jethava@users.noreply.github.com>
2022-04-19 16:08:47 +0530

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,14 +153,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
 STATIC_URL = '/static/'
 # MEDIA_URL='/media/'
 #for v2
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 #for v2
 STATICFILES_FINDERS = [
