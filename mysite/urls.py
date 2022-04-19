66a2ed6479a998a936f6e95ef6d01aaa5c51091b
sanket-jethava <74979818+sanket-jethava@users.noreply.github.com>
2022-04-19 16:02:09 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,9 +4,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.views.static import serve 
+from django.conf import settings
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
