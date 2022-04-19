46f53fa70c2ac1af72c4291898e9e32991192b61
sanket-jethava <74979818+sanket-jethava@users.noreply.github.com>
2022-04-19 16:00:23 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,7 +43,8 @@ SECRET_KEY = config(
 # DEBUG = True
 
 # v2
-DEBUG = config("DEBUG", default=True, cast=bool)
+# DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -151,10 +152,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 #for v2
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
